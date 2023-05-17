c36ece56a234fafbe1a11a017baf247c767d9ab9
AizeN1337 <maciejkalata79@gmail.com>
2023-05-17 21:19:45 +0200

    modified:   src/env.mjs

--- src/env.mjs
+++ src/env.mjs
@@ -26,7 +26,6 @@ const client = z.object({
 const processEnv = {
   DATABASE_URL: process.env.DATABASE_URL,
   NODE_ENV: process.env.NODE_ENV,
-  // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
 // Don't touch the part below
