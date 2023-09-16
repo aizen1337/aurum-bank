ec272737125a0461490cde3037dbfefa5ba14ec0
AizeN1337 <maciejkalata79@gmail.com>
2023-09-17 00:20:05 +0200

    fixed ssr, working demo

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,19 +1,23 @@
 import Sidebar from "@/components/Sidebar/Sidebar";
-import { currentUser } from "@clerk/nextjs";
+import { RedirectToSignIn, SignedIn, SignedOut } from "@clerk/nextjs";
 export default async function DashboardLayout({
     children, 
   }: {
     children: React.ReactNode;
   }) {
-    const user = await currentUser()
-    return user && (
+    return  (
       <div>
+      <SignedIn>
               <main className="flex min-h-screen max-w-screen xl:items-center justify-center w-screen xl:h-full bg-gradient-to-b from-[#252525] to-[#141414]">
                   <Sidebar/>
                   <section className="xl:ml-48 xl:w-full xl:flex lg:items-center mb-20 xl:mb-0 xl:justify-center">
                   {children}
                   </section>
               </main>
+      </SignedIn>
+      <SignedOut>
+        <RedirectToSignIn afterSignInUrl={'/dashboard'} afterSignUpUrl={`/dashboard`}/>
+      </SignedOut>
       </div>
     );
   }
